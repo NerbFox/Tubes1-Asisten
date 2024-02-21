8ee10b5802d986271f54654129ae0aa12794aa40
Nigel Sahl <93074692+NerbFox@users.noreply.github.com>
2024-02-21 17:57:53 +0700

    Setup Bot

---
+++ run-bots.bat
@@ -0,0 +1,5 @@
+@echo off
+start cmd /c "python main.py --logic Random --email=test@email.com --name=stima --password=123456 --team etimo"
+start cmd /c "python main.py --logic Random --email=test1@email.com --name=stima1 --password=123456 --team etimo"
+start cmd /c "python main.py --logic Random --email=test2@email.com --name=stima2 --password=123456 --team etimo"
+start cmd /c "python main.py --logic Asisten --email=test4@email.com --name=Asisten --password=1256 --team etimo"
