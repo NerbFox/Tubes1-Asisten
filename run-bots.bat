72ead70cd5ad934f2669dacb7b2077441da2d6a2
Nigel Sahl <93074692+NerbFox@users.noreply.github.com>
2024-03-18 15:21:53 +0700

    Update run-bots.bat

--- run-bots.bat
+++ run-bots.bat
@@ -3,3 +3,12 @@ start cmd /c "python main.py --logic Random --email=test@email.com --name=stima 
 start cmd /c "python main.py --logic Random --email=test1@email.com --name=stima1 --password=123456 --team etimo"
 start cmd /c "python main.py --logic Random --email=test2@email.com --name=stima2 --password=123456 --team etimo"
 start cmd /c "python main.py --logic Asisten --email=test4@email.com --name=Asisten --password=1256 --team etimo"
+
+    
+start cmd /c "python main.py --logic ArticaKenapaBatal --email=ArticaKenapaBatal@email.com --name=ArticaKenapaBatal --password=123456 --team etimo"
+start cmd /c "python main.py --logic Cijawjaw --email=Cijawjaw@email.com --name=Cijawjaw --password=123456 --team etimo"
+start cmd /c "python main.py --logic DNA --email=DNA@email.com --name=DNA --password=123456 --team etimo"
+start cmd /c "python main.py --logic Dujari --email=Dujari@email.com --name=Dujari --password=123456 --team etimo"
+start cmd /c "python main.py --logic QUEEN --email=QUEEN@email.com --name=QUEEN --password=123456 --team etimo"
+start cmd /c "python main.py --logic terkesTima --email=terkesTima@email.com --name=terkesTima --password=123456 --team etimo"
+start cmd /c "python main.py --logic TheYuds --email=TheYuds@email.com --name=TheYuds --password=123456 --team etimo"
